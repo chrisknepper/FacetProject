cc2cf630470ce5a6c6752ce93f6b520cde716e9b
cmbackus <cbackus123@gmail.com>
2015-03-08 12:57:45 -0700

    fixed to query different db, not using form

--- myApp/www/js/post.js
+++ myApp/www/js/post.js
@@ -1,23 +1,34 @@
-$(function(){
-        $('form').submit(function(){
-            var landmarkID = $(this).parent().attr('data-landmark-id');
-            var postData = $(this).serialize();
-            var postData2 = 'MAGICWATCH';
-            
-            $.ajax({
-                type: 'POST',
-                data: postData2,
-                url: 'http://banjoboygames.com/test/save.php',
-                success: function(data){
-                    console.log(data);
-                    alert('Your comment was successfully added');
-                },
-                error: function(){
-                    console.log(data);
-                    alert('There was an error adding your comment');
-                }
-            });
-            
-            return false;
-        });
-    });
+$(document).ready(init);
+	function init(){
+		//console.log($('#searchbar').val());
+		
+		$('#SaveButton').click(runWatchSearch);
+		
+		
+	}
+	function runWatchSearch(){
+					var FETCH_TRIPLES_URL = "http://people.rit.edu/cmb7743/FacetTests/php/fetchtriples.php?n=";
+					var value = $.trim($('#comment').val());
+					
+					var url =  FETCH_TRIPLES_URL+value;
+					console.log(url);
+					$.getJSON(url).done(function(data){
+					
+					watchDataLoaded(data);});
+					}
+					
+function watchDataLoaded(data){
+						if(data.results){
+						var results = data.results;
+						var current=results[0];
+						if(results.length!=0){
+						
+						$('#Title').text(current.productTitle);
+					
+						}
+						else{
+						$('#Title').text("no results found");
+						}
+						
+						}
+	}
