406988d760a87e23f8f2fc5615ff1f775c87f053
Chris Knepper <cxk1108@rit.edu>
2015-04-09 09:49:20 -0400

    -Clean UP

--- facetMerge/client/js/main.js
+++
@@ -1,6 +0,0 @@
-var container = document.querySelector('#container');
-var msnry = new Masonry( container, {
-	// options
-	columnWidth: 212,
-	itemSelector: '.item'
-});
--- facetMerge/src/controllers/Product.js
+++
@@ -1,8 +0,0 @@
-
-var updateProduct = function(req, res) {
-    console.log("update product");
-	res.render('simulate');
-};
-
-
-module.exports.updateProduct = updateProduct;
