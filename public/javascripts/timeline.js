575435204e5cc90de49977fb1a3dd0679954b420
Charlie Backus <cbackus123@gmail.com>
2015-04-27 20:22:06 -0400

    product and timeline
    product and timeline load dynamically.
    some css.
    icons on timeline are wonky.
    db tables added

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -6,12 +6,12 @@ function init() {
 		$('#model iframe').attr('src', 'https://sketchfab.com/models/04e3d5fe3f9046aa9f2ab62ffece4fce/embed?autostart=1');
 	});
 	var container = document.querySelector('#container');
-    $('#movementm').click(function(){switchMenu('#movement');});
-    $('#featuresm').click(function(){switchMenu('#features');});
-    $('#variationsm').click(function(){switchMenu('#variations');});
-    $('#inspirationm').click(function(){switchMenu('#inspirations');});  
-    $('#externalm').click(function(){switchMenu('#external');});
-    $('#buym').click(function(){switchMenu('#buy');});
+    $('#watchesm').click(function(){switchMenu('#watches');});
+    $('#technologym').click(function(){switchMenu('#technology');});
+    $('#eventm').click(function(){switchMenu('#event');});
+    $('#iconm').click(function(){switchMenu('#icons');});  
+    $('#historym').click(function(){switchMenu('#history');});
+    $('#achievementsm').click(function(){switchMenu('#achievements');});
     $('.back').click(function(){
         //var category=this.parent();
          $(this).parent().css('zIndex', '-1');
--- routes/timeline.js
+++ routes/timeline.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-	res.render('timeline');
+    res.render('timeline', {specs:{name: 'Default Title', collection: 'Academy', frequency:'3600', name:'Lightweight', shortname:" short", thumb:"imgurl", largeImg:"imgurl",model:"modelurl", price:"8,500", movement:"El Primero Automatic 45B", calibre:"13 1/4", thickness:"7.35", components:"341", jewels:"53", frequency:"28800", powerReserve:"Min 50 hrs.", finishing:"Shiny", icon:"Felix", material:"stainless steel", diameter: "43", crystal:"domed sapphire", caseBack:"transparent mythril", strap:"Black alligator leather", clasp:"Stainless Steel", waterResistance:"8", variations:"6", productionModel:"2013" }});
+
 });
 
 
