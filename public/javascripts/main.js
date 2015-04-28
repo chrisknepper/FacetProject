ae91986d72924573dd657d71a06051ebdc610124
Chris Knepper <cxk1108@rit.edu>
2015-04-28 08:38:52 -0400

    -Add second masonry area on home page -Change to create masonry in loop -Fix text under shadows on home page -Fix broken 4-image callout on home page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,10 @@
 function init() {
-	var container = document.querySelector('#container');
-	
-	var msnry = new Masonry( container, {
-		// options
-		columnWidth: 280,
-		gutter: 10
-	});
-
+	var container = document.querySelectorAll('.tileContainer');
+	for (var i = 0; i < container.length; i++) {
+		var msnry = new Masonry( container[i], {
+			// options
+			columnWidth: 280,
+			gutter: 10
+		});
+	};
 }
