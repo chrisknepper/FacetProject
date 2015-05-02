eb6ad78d17e7dd4ae283e6f40d5834b35da07ea5
Chris Knepper <cxk1108@rit.edu>
2015-05-02 10:51:15 -0400

    -Lighting and rendering

--- public/javascripts/threedemo.js
+++ public/javascripts/threedemo.js
@@ -41,7 +41,7 @@
         controls.maxZoom                  = 80;
 
           var loader = new THREE.OBJMTLLoader();
-  loader.load( '/models/finalwatchmodel.obj', '/models/finalwatchmodel.mtl', function ( object ) {
+  loader.load( '/models/finalwatchmodelthree.obj', '/models/finalwatchmodel.mtl', function ( object ) {
 
     object.traverse( function ( child ) {
 
